cd4961c140b36c06f028083ce2581f2809ec0744
Alsender <xlstadal@gmail.com>
2022-07-25 20:15:23 +0200

    added german

--- pixelwords.js
+++ pixelwords.js
@@ -1,5 +1,10 @@
 var toBePainted = []
+var toBeUndone = []
 var counter = 0
+
+var verb
+var person
+var tense
 
 function chooseColor(target) {
     if (document.querySelectorAll('.activeColor').length != 0) {
@@ -8,19 +13,13 @@ function chooseColor(target) {
     chosenColor = crayola[(target.id).substring(5)]
     target.classList.toggle('activeColor')
 }
-function choose2ndColor(target) {
-    if (document.querySelectorAll('.active2ndColor').length != 0) {
-        document.querySelectorAll('.active2ndColor')[0].classList.toggle('active2ndColor')
-    }
-    chosen2ndColor = crayola[(target.id).substring(5)]
-    target.classList.toggle('active2ndColor')
-}
 function paint(target) {
-    if (target.dataset.color == chosenColor) {
-        target.style.removeProperty('background-color')
-        counter--
-        return
-    } else
+    for (i = 0; i < toBePainted.length; i++) {
+        if (target === toBePainted[i]) {
+            remove(target)
+            return
+        }
+    }
     if (counter < 3) {
         if (chosenColor === undefined) {
             return
@@ -29,28 +28,49 @@ function paint(target) {
         target.dataset.color = chosenColor
         target.style.backgroundColor = chosenColor+50
         counter++
+        toBeUndone.push(target)
+    }
+    if (counter == 3) {
+        randomize()
     }
 }
-function paint2nd(target) {
-    if (target.dataset.color == chosen2ndColor) {
-        target.style.removeProperty('background-color')
-        counter--
-        return
-    } else
-    if (counter < 3) {
-        if (chosen2ndColor === undefined) {
-            return
-        } else
-        toBePainted.push(target)
-        target.dataset.color = chosen2ndColor
-        target.style.backgroundColor = chosen2ndColor+50
-        counter++
-    }
+function keyup(event) {
+    if (event.keyCode === 90 && event.ctrlKey) undo();
+    else if (event.keyCode === 13) confirm();
+    else return
 }
 function confirm() {
-    for (i = 0; i <= toBePainted.length - 1; i++) {
-        toBePainted[i].style.backgroundColor = toBePainted[i].dataset.color
+    console.log(verb,person,tense)
+    check = conjugate(...Object.values(verbs[verb]),person,tense).replace(/\s/g, ' +[\\s/\\S]+ ').split(",")
+    console.log(check)
+    for (i=0;i<check.length;i++) {
+        if (textBox.value.match(check[i]) == null) {
+            return false
+        } else 
+        for (i = 0; i <= toBePainted.length - 1; i++) {toBePainted[i].style.backgroundColor = toBePainted[i].dataset.color}
+        toBePainted = []
+        toBeUndone = []
+        counter = 0
     }
-    toBePainted = []
-    counter = 0
+//    if (check.some(c => textBox.value.match(c))) {
+//        for (i = 0; i <= toBePainted.length - 1; i++) {
+//            toBePainted[i].style.backgroundColor = toBePainted[i].dataset.color
+        
+//    }
+}
+function remove(target) {
+    for (i = 0; i < toBePainted.length; i++) {
+        if (target === toBePainted[i]) {
+            counter--
+            toBePainted.splice(i, 1)
+            target.removeAttribute('data-color')
+            target.style.removeProperty('background-color')
+        }
+    }
+    return
+}
+function undo() {
+    target = toBeUndone[toBeUndone.length - 1]
+    toBeUndone.splice(toBeUndone.length - 1, 1)
+    remove(target)
 }
